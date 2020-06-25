0307bc562172beb389b5ed0507c8df991bc8a3fd
Tomás Migone <tomasmigone@gmail.com>
2020-06-25 10:49:04 -0300

    entrypoint changes
    Change-type: patch
    Signed-off-by: Tomás Migone <tomasmigone@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,32 @@
 #!/bin/sh -l
 set -e
 
+function get_version () {
+  # Get version
+  if [[ -f "VERSION" ]]; then
+    VERSION="v$(cat VERSION)"
+  elif [[ -f "package.json" ]]; then
+    VERSION="v$(jq -r .version package.json)"
+  else
+    echo "No VERSION or package.json file to get version from."
+    exit 1
+  fi
+}
+
 # Ensure we have a repo.yml file
 if [ ! -f repo.yml ]; then
   echo "repo.yml not found, creating generic..."
-    echo "type: generic" > repo.yml
+  echo "type: generic" > repo.yml
 fi
+
+git tag
+get_version
+echo $VERSION
 
 # Run versionist
 balena-versionist
 
-# Get version
-if [[ -f "VERSION" ]]; then
-  VERSION="v$(cat VERSION)"
-elif [[ -f "package.json" ]]; then
-  VERSION="v$(jq -r .version package.json)"
-else
-  echo "No VERSION or package.json file to get version from."
-  exit 1
-fi
+get_version
 
 # Commit and push changes
 git config --local user.email "$INPUT_GITHUB_EMAIL"
